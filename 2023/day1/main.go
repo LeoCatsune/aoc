3abc8be3ca2c1363519b22f4c674b66e05f9af3e
Leo <git@leocatto.com>
2024-12-02 00:22:10 +1000

    2023 Day 1 (2/2)

--- 2023/day1/main.go
+++ 2023/day1/main.go
@@ -5,9 +5,11 @@ import (
 	"fmt"
 	"log"
 	"os"
-	"regexp"
+	"slices"
 	"strconv"
 	"time"
+
+	"github.com/dlclark/regexp2"
 )
 
 func main() {
@@ -18,26 +20,49 @@ func main() {
 	}
 	defer file.Close()
 
-	first := regexp.MustCompile(`^.*?(\d)`)
-	last := regexp.MustCompile(`.*(\d).*?$`)
+	// because fuck you, I'm not using a loop.
+	num := regexp2.MustCompile(`(?=(\d|one|two|three|four|five|six|seven|eight|nine))`, regexp2.RE2)
+	words := []string{"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"}
 
 	sum := 0
 
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
-		f, err := strconv.Atoi(first.FindStringSubmatch(scanner.Text())[1])
-		if err != nil {
-			log.Fatal(err)
+		nums := regexp2FindAllString(num, scanner.Text())
+		first := nums[0]
+		last := nums[len(nums)-1]
+
+		fv := slices.Index(words, first)
+		if fv == -1 {
+			fv, err = strconv.Atoi(first)
+			if err != nil {
+				log.Fatal(err)
+			}
 		}
 
-		l, err := strconv.Atoi(last.FindStringSubmatch(scanner.Text())[1])
-		if err != nil {
-			log.Fatal(err)
+		lv := slices.Index(words, last)
+		if lv == -1 {
+			lv, err = strconv.Atoi(last)
+			if err != nil {
+				log.Fatal(err)
+			}
 		}
 
-		sum += (f * 10) + l
+		fmt.Printf("%s (%s) (%s) => %d%d\n", scanner.Text(), first, last, fv, lv)
+
+		sum += (fv * 10) + lv
 	}
 
 	println("Sum:", sum)
 	fmt.Printf("(Took %dms)", time.Since(start)/1000)
+}
+
+func regexp2FindAllString(re *regexp2.Regexp, s string) []string {
+	var matches []string
+	m, _ := re.FindStringMatch(s)
+	for m != nil {
+		matches = append(matches, m.GroupByNumber(1).String())
+		m, _ = re.FindNextMatch(m)
+	}
+	return matches
 }
