3bef972ff6de1239ca14b6a09848178be4326568
Leo <git@leocatto.com>
2024-12-04 20:51:07 +1000

    2024 Day 4 (2/2)

--- 2024/day4/main.py
+++ 2024/day4/main.py
@@ -3,6 +3,7 @@
 
 def main():
 	total = 0
+	newTotal = 0
 	file = open("input.txt", "r")
 	data = file.readlines()
 
@@ -10,8 +11,11 @@ def main():
 		for y in range(len(data[0])):
 			if data[x][y] == "X":
 				total += findXmas(data, x, y)
+			if data[x][y] == "A":
+				newTotal += findX_MAS(data, x, y)
 
-	print("total:", total)
+	print("total(1):", total)
+	print("total(2):", newTotal)
 
 # Check Adjacent Cells for XMAS
 def findXmas(arr, x, y) -> int:
@@ -31,6 +35,29 @@ def findXmas(arr, x, y) -> int:
 				# print("Found XMAS: %d,%d to %d,%d (%d, %d)" % (x, y, x+(3*i), y+(3*j), i, j))
 				found += 1
 	return found
+
+# Check Adjacent Cells for MAS in an X shape (X-MAS)
+def findX_MAS(arr, x, y) -> int:
+	found = 0
+	checks = [
+		((-1, -1), (1, 1)), ((-1, 1), (1, -1)),
+		((1, -1), (-1, 1)), ((1, 1), (-1, -1))
+	]
+
+	if arr[x][y] != "A":
+		return 0
+	
+	for (i, j), (k, l) in checks:
+		# edge cases (but longer)
+		if x+i not in range(len(arr))\
+			or x+k not in range(len(arr))\
+			or y+j not in range(len(arr[0]))\
+			or y+l not in range(len(arr[0])):
+				continue
+		if arr[x+i][y+j] == "M" and arr[x+k][y+l] == "S":
+			found += 1
+	
+	return int(found == 2)
 
 
 if __name__ == "__main__":
